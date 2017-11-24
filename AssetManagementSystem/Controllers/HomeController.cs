b4c747d5a67c99d9b1f3dbf7a685b49c2134c562
Shubham Jolapara <Shubham Jolapara@INFINITEDEVT-17>
2017-11-24 20:07:20 +0530

    CMDB2:Commit On HomeController

--- AssetManagementSystem/Controllers/HomeController.cs
+++ AssetManagementSystem/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+//homecontroller changes
 namespace AssetManagementSystem.Controllers
 {
     public class HomeController : Controller
